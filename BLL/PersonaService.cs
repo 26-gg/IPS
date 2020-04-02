3be6de0967f4abc2d03e61691f68be9d6aca78e7
26-gg <63007456+26-gg@users.noreply.github.com>
2020-04-02 11:02:49 -0500

    Repositorio de la ips mas salud

---
+++ BLL/PersonaService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using Entity;
+
+namespace BLL
+{
+    public class PersonaService
+    {
+        PersonaRepository PersonaRepositorio   = new PersonaRepository();
+        public String Guardar(Persona persona)
+        {
+            if (PersonaRepositorio.BuscarIdentificacion(persona.Identificacion)==null)
+            {
+                PersonaRepositorio.Guardar(persona);
+                return $"se guardo la persona con la identificacion {persona.Identificacion}";
+            }
+            return $"la persona con identificacion {persona.Identificacion} no se guardo";
+    
+
+        }
+
+        public String Eliminar(Persona persona)
+        {
+            if (persona== null)
+            {
+                return $"la persona {persona.Identificacion} no se encuentra registrada ";
+            }
+            if (PersonaRepositorio.BuscarIdentificacion(persona.Identificacion)==null)
+            {
+                return $"la identificacion {persona.Identificacion}la persona no se encuentra registrada ";
+            }
+            PersonaRepositorio.Eliminar(persona);
+            return $"la identificacion {persona.Identificacion}se elimino correctamente";
+        }
+
+        public List<Persona> Consultar()
+        {
+            return PersonaRepositorio.Consultar();
+        }
+
+        public Persona CalcularPulsaciones(Persona persona) 
+        {
+            persona.CalcularPulsaciones();
+            return persona;
+        }
+        public Persona Buscar(string id)
+
+        {
+            return PersonaRepositorio.BuscarIdentificacion(id);
+        }
+
+
+    }
+}
---
+++ CapaPresentecion/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BLL;
+using Entity;
+
+
+namespace CapaPresentecion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            string identificacion;
+            string sexo;
+            int edad;
+            Persona persona;
+            ConsoleKeyInfo opcion;
+            PersonaService Servicio = new PersonaService();
+            do
+            {
+                try
+                {
+                    Console.Clear();
+                    Console.WriteLine("digite sus datos");
+                    Console.WriteLine("digite su nombre");
+                    nombre = Console.ReadLine();
+                    Console.WriteLine("digite su identificacion");
+                    identificacion = Console.ReadLine();
+                    Console.WriteLine("digite el sexo");
+                    sexo = Console.ReadLine();
+                    Console.WriteLine("digite su edad");
+                    edad = Convert.ToInt32(Console.ReadLine());
+
+                    persona = new Persona(identificacion, nombre, sexo, edad);
+
+                    Servicio.CalcularPulsaciones(persona);
+                    Console.WriteLine(Servicio.Guardar(persona));
+                    persona.ToString();
+
+                }
+                catch (Exception e)
+                {
+
+                    Console.WriteLine("Error" + e.Message);
+                }
+                Console.WriteLine("Desea continuar (s/n)");
+                opcion = Console.ReadKey();
+
+            } while (opcion.Key == ConsoleKey.S);
+
+            Console.Clear();
+            Console.WriteLine("Consulta de pulsaciones");
+            foreach (var item in Servicio.Consultar()) ;
+            {
+                Console.WriteLine(item.ToString());
+            }
+            Console.WriteLine("Digite la identificacion a eliminar");
+            identificacion = Console.ReadLine();
+            Console.WriteLine(Servicio.Eliminar(Servicio.Buscar(identificacion)));
+
+            Console.WriteLine("Consulta de pulsaciones");
+            foreach (var item in Servicio.Consultar())
+            {
+                Console.WriteLine(item.ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
